91a3645865863abb89c154bf84f57ed466679db8
Artúr Friedrich <artur.frierdrich.harka@gmail.com>
2021-10-26 17:17:30 +0200

    Update modularis_hatvanyozas.py

--- egyéb/diszkrét_matek/modularis_hatvanyozas.py
+++ egyéb/diszkrét_matek/modularis_hatvanyozas.py
@@ -1,5 +1,6 @@
 from math import *
 
+print(pow(2, 6))
 # alap = int(input("Adja meg az alapot: "))
 # alap_hatvany = int(input("Adja meg az alap hatványát: "))
 # alap_oszto = int(input("Adja meg az osztót: "))
@@ -16,12 +17,25 @@ for i in range(10):
     res = pow(results[i], 2) % alap_oszto
     results.append(res)
 
-# kettő hatványainak megtalálása
-powers = [2]
+# kettő hatványai
+powers = [1, 2]
 power = 2
-while power < alap_hatvany:
+while power < alap_hatvany - power:
     powers.append(power * 2)
     power *= 2
-print(powers)
 
-print(results)
+z = list()
+
+powers = list(reversed(powers))
+for i in range(len(powers)):
+    if alap_hatvany - powers[i] >= 0:
+        alap_hatvany = alap_hatvany - powers[i]
+        z.append(i)
+
+sum = 1
+for i in range(len(z)):
+    print(results[z[i]])
+    sum *= results[z[i]]
+
+end = sum % alap_oszto
+print("End:", end)
