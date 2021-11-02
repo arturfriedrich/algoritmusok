8fcac2eeb4194a3e5f885cbc7b66aeeeb05f13ff
Artúr Friedrich <artur.frierdrich.harka@gmail.com>
2021-11-02 11:38:39 +0100

    Finished

--- egyéb/diszkrét_matek/modularis_hatvanyozas.py
+++ egyéb/diszkrét_matek/modularis_hatvanyozas.py
@@ -1,13 +1,9 @@
 from math import *
 
-print(pow(2, 6))
-# alap = int(input("Adja meg az alapot: "))
-# alap_hatvany = int(input("Adja meg az alap hatványát: "))
-# alap_oszto = int(input("Adja meg az osztót: "))
-
-alap = 52
-alap_hatvany = 79
-alap_oszto = 253
+alap = int(input("Adja meg az alapot: "))
+alap_hatvany = int(input("Adja meg az alap hatványát: "))
+alap_oszto = int(input("Adja meg az osztót: "))
+hatvany = alap_hatvany
 
 results = list()
 results.append(alap)
@@ -32,10 +28,12 @@ for i in range(len(powers)):
         alap_hatvany = alap_hatvany - powers[i]
         z.append(i)
 
-sum = 1
 for i in range(len(z)):
-    print(results[z[i]])
-    sum *= results[z[i]]
+    z[i] = max(z) - z[i]
 
-end = sum % alap_oszto
-print("End:", end)
+összeg = 1
+for i in range(len(z)):
+    összeg *= results[z[i]]
+
+end = összeg % alap_oszto
+print(f"A(z) {alap} a(z) {hatvany}. hatványon-nak vett maradéka {alap_oszto}-el:", end)
